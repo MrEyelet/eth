e4a1f9ee484871824df8de0f1ac87f9915c0cf05
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-10 20:48:37 +0100

    prelaoder v1

--- js/modules/AppearElements.js
+++ js/modules/AppearElements.js
@@ -14,6 +14,7 @@ class AppearElements {
     document.addEventListener("DOMContentLoaded", () => {
       window.onload = () => {
         window.requestAnimationFrame(() => {
+          window.scrollTo(0, 0)
           this.showElements()
         })
       }
@@ -28,11 +29,13 @@ class AppearElements {
     this.tlShowElements.from(".hero__intro h1", { duration: 1, scale: 1.1, opacity: 0, ease: Power1.easeOut }, "=-1.4")
     this.tlShowElements.from(".hero__img-overlay", { duration: 1, scaleX: 0, transformOrigin: "left", ease: Power4.easeOut }, "=-1.2")
     this.tlShowElements.to(".hero__img img", { duration: 0, opacity: 1, ease: Power4.easeOut })
-    this.tlShowElements.to(".hero__img-overlay", { duration: 0.5, scaleX: 0, transformOrigin: "right", ease: Power4.easeOut })
-    this.tlShowElements.from(".header", { duration: 0.35, y: "-100%", ease: Power1.easeOut }, "=-1.5")
+    this.tlShowElements.to(".hero__img-overlay", { duration: 1, scaleX: 0, transformOrigin: "right", ease: Power4.easeOut })
+    this.tlShowElements.to(".subscribe__form-group", { duration: 1, width: "100%", opacity: 1, ease: Power4.easeOut }, "=-1.1")
+    this.tlShowElements.from(".subscribe__title", { duration: 1.5, opacity: 0, ease: Power4.easeOut }, "=-1.1")
+    this.tlShowElements.from(".header", { duration: 0.35, y: "-100%", ease: Power1.easeOut }, "=-2")
     setTimeout(() => {
       this.body.classList.remove("scroll-disabled")
-    }, 500)
+    }, 600)
     setTimeout(() => {
       this.preloader.remove()
     }, 2500)
