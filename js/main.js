2d5b9a42aeac33555900ee4d68c48584c3cfdf3f
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-10 20:12:01 +0100

    preloader styling

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // Modules / classes
+import AppearElements from "./modules/AppearElements"
 import Slider from "./modules/Slider"
 import Modal from "./modules/Modal"
 import Video from "./modules/Video"
 
 // Instantiate a new object using our modules/classes
+var appearElements = new AppearElements()
 var slider = new Slider()
 var modal = new Modal()
 var video = new Video()
---
+++ js/modules/AppearElements.js
@@ -0,0 +1,42 @@
+import { gsap, TimelineMax, Power1, Power4 } from "gsap"
+gsap.registerPlugin()
+
+class AppearElements {
+  constructor() {
+    this.body = document.querySelector("body")
+    this.preloader = document.querySelector(".preloader")
+    this.tlShowElements = new TimelineMax({})
+
+    this.events()
+  }
+
+  events() {
+    document.addEventListener("DOMContentLoaded", () => {
+      window.onload = () => {
+        window.requestAnimationFrame(() => {
+          this.showElements()
+        })
+      }
+    })
+  }
+
+  showElements() {
+    this.tlShowElements.from(".preloader__cover", { duration: 1.5, x: "200%", ease: Power1.easeOut })
+    this.tlShowElements.to(".preloader__outer-circle", { duration: 0.5, opacity: 0, ease: Power4.easeOut }, "=-1.5")
+    this.tlShowElements.to(".preloader", { duration: 1.5, x: "-100%", ease: Power4.easeOut }, "=-1")
+    this.tlShowElements.from(".shape", { duration: 1.5, rotate: "-25deg", transformOrigin: "top left", stagger: 0.15, ease: Power4.easeOut }, "=-1.4")
+    this.tlShowElements.from(".hero__intro h1", { duration: 1, scale: 1.1, opacity: 0, ease: Power1.easeOut }, "=-1.4")
+    this.tlShowElements.from(".hero__img-overlay", { duration: 1, scaleX: 0, transformOrigin: "left", ease: Power4.easeOut }, "=-1.2")
+    this.tlShowElements.to(".hero__img img", { duration: 0, opacity: 1, ease: Power4.easeOut })
+    this.tlShowElements.to(".hero__img-overlay", { duration: 0.5, scaleX: 0, transformOrigin: "right", ease: Power4.easeOut })
+    this.tlShowElements.from(".header", { duration: 0.35, y: "-100%", ease: Power1.easeOut }, "=-1.5")
+    setTimeout(() => {
+      this.body.classList.remove("scroll-disabled")
+    }, 500)
+    setTimeout(() => {
+      this.preloader.remove()
+    }, 2500)
+  }
+}
+
+export default AppearElements
