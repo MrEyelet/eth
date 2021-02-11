44991a14145c11d83da9b44239f5333e911b95af
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-11 09:22:13 +0100

    menu spy

--- js/modules/ActiveLink.js
+++ js/modules/ActiveLink.js
@@ -1,20 +1,28 @@
+var MenuSpy = require("menuspy")
+
 class ActiveLink {
   constructor() {
     this.nav = document.querySelector(".nav")
-    this.links = this.nav.querySelectorAll("a")
-
-    this.toggleActiveLink()
+    this.links = this.nav.querySelectorAll(".nav__list-el")
+    // console.log(MenuSpy)
+    this.menuSpy()
+    // this.toggleActiveLink()
   }
 
-  toggleActiveLink() {
-    for (let i = 0; i < this.links.length; i++) {
-      this.links[i].addEventListener("click", e => {
-        let current = document.getElementsByClassName("active")
-        current[0].className = current[0].className.replace(" active", "")
-        e.currentTarget.className += " active"
-      })
-    }
+  menuSpy() {
+    let elm = document.querySelector("#nav")
+    let ms = new MenuSpy(elm)
   }
+
+  // toggleActiveLink() {
+  //   for (let i = 0; i < this.links.length; i++) {
+  //     this.links[i].addEventListener("click", e => {
+  //       let current = document.getElementsByClassName("active")
+  //       current[0].className = current[0].className.replace(" active", "")
+  //       e.currentTarget.className += " active"
+  //     })
+  //   }
+  // }
 }
 
 export default ActiveLink
